f38ec727d1e9c52e94826adfc917ac09ee7b2663
unknown <divyansh8252230@gmail.com>
2021-06-22 21:48:16 +0530

    Updates on Quiz

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const quizDb = [
         b: "hey text markup language",
         c: "hypertext makeup language",
         d: "hypertext markup language",
-        ans: "ans4"
+        ans: "ans4",
+        selected: ''
     },
     {
         question : "Q2 : what is the full form of cc ?",
@@ -13,7 +14,8 @@ const quizDb = [
         b: "Cascading Style Sheep",
         c: "Cartoon Style Sheet",
         d: "Cascading Super Sheet",
-        ans: "ans1"   
+        ans: "ans1"  ,
+        selected: '' 
     },
     {
         question : "Q3 : what is the full form of html ?",
@@ -21,7 +23,8 @@ const quizDb = [
         b: "hey text markup language",
         c: "hypertext makeup language",
         d: "hypertext markup language",
-        ans: "ans4"
+        ans: "ans4",
+        selected: ''
     },
     {
         question : "Q4 : what is the full form of cc ?",
@@ -29,7 +32,26 @@ const quizDb = [
         b: "Cascading Style Sheep",
         c: "Cartoon Style Sheet",
         d: "Cascading Super Sheet",
-        ans: "ans1"   
+        ans: "ans1" ,
+        selected: ''  
+    },
+    {
+        question : "Q5 : what is the full form of cc ?",
+        a: "Cascading Style Sheets",
+        b: "Cascading Style Sheep",
+        c: "Cartoon Style Sheet",
+        d: "Cascading Super Sheet",
+        ans: "ans1",
+        selected: ''   
+    },
+    {
+        question : "Q6 : what is the full form of cc ?",
+        a: "Cascading Style Sheets",
+        b: "Cascading Style Sheep",
+        c: "Cartoon Style Sheet",
+        d: "Cascading Super Sheet",
+        ans: "ans1",
+        selected: '' 
     },
 ];
 
@@ -40,6 +62,8 @@ const option3 = document.querySelector('#option3');
 const option4 = document.querySelector('#option4');
 const submit = document.querySelector('#submit');
 const answers = document.querySelectorAll('.answer');
+const labels = document.getElementsByTagName('label');
+const back = document.getElementById('back');
 
 const uname = document.querySelector('.uname');
 const psw = document.querySelector('.psw');
@@ -53,11 +77,108 @@ const agree = document.querySelector('.agree');
 const startQuiz = document.querySelector('#startQuiz');
 const agreeBox = document.querySelector('#agreeBox');
 
+// Result
+const result = document.querySelector('.result')
+let userAns = new Array();
+let bool = new Array();
+const table = document.querySelector("#customers");
+const showResult = document.querySelectorAll('.showResult');
+const ur = document.getElementById('urResult');
+const um = document.getElementById('urmedian');
+const qn = document.getElementById('qn');
+const wa = document.getElementById('wa');
+const ca = document.getElementById('ca');
+const foot = document.querySelector('.foot');
+
+
+function displayData(){
+
+    let rs=0,total=0,wrong=0;
+    for(let i=0;i<quizDb.length;i++)
+    {
+        if(bool[i])
+        {
+
+            rs++;
+        }
+        else{
+            wrong++;
+        }
+        
+    }
+    total = (rs/quizDb.length)*100;
+     total = total.toFixed(2);
+    if(total < 50)
+    {        
+        showResult[0].style.background = '#f2dede';
+        showResult[0].style.color = '#a94442';
+        foot.innerHTML = 'Upsss !! You need Serious Improvement'
+    }
+    else if(total >= 50 && total <=80)
+    {
+        showResult[0].style.background = 'yellow';
+        foot.innerHTML = 'Good Job !! You can do Better'
+    }
+    else{
+        showResult[0].style.background = '#dff0d8';
+        showResult[0].style.color = '#3c763d';
+        foot.innerHTML = 'Excellent Job !! You are doing Great'
+    }
+    ur.innerHTML = `Your Result is: ${total}% `;
+
+    qn.innerHTML = `Total number of Questions : ${quizDb.length}`;
+    wa.innerHTML = `Total number of Wrong Answer : ${wrong}`
+    ca.innerHTML = `Total number of Correct Answer : ${rs}`;
+
+
+    for(let i=0; i< quizDb.length; i++)
+    {
+      let row = table.insertRow(i+1)
+      let cell1 = row.insertCell(0);
+      let cell2 = row.insertCell(1);
+      let cell3 = row.insertCell(1);
+      cell1.innerHTML = i+1;
+      cell3.innerHTML = userAns[i];
+      cell2.innerHTML = bool[i] + " Mark";
+       if(bool[i])
+       {
+           row.style.background = '#dff0d8';
+           row.style.borderColor = '#d6e9c6';
+           row.style.color = '#3c763d';
+       } 
+       else{
+        row.style.background = '#f2dede';
+        row.style.borderColor = '#ebccd1';
+        row.style.color = '#a94442';
+       }
+    }
+}
 
 let questionCount =0;
 let score =0 ;
 
+
+
 const loadQuestion = () => {
+    if(questionCount === quizDb.length-1)
+    {
+        submit.innerHTML = "Submit"
+    }
+
+    if(questionCount === 0)
+        {
+            back.disabled = true;
+            back.style.cursor = 'not-allowed';
+        }
+        else{
+            back.disabled = false;
+            back.style.cursor = '';
+        }
+
+    if(quizDb[questionCount].selected)
+    {
+        document.getElementById(quizDb[questionCount].selected).checked = true;
+    }
 
     const questionList = quizDb[questionCount];
     question.innerHTML = questionList.question;
@@ -84,6 +205,7 @@ const getCheckAnswer = () => {
 
            
     });
+    
     return answer;
 
 }
@@ -97,25 +219,60 @@ const deselectAll = () => {
     })
 }
 
+back.addEventListener('click',()=>{
+    questionCount--;
+    loadQuestion();
+})
+
 submit.addEventListener('click', () => {
     const checkedAnswer = getCheckAnswer();
+    
+    quizDb[questionCount].selected = checkedAnswer;
 
-    ///console.log(checkedAnswer);
+    if(checkedAnswer === 'undefined')
+    {
+        userAns[questionCount] = checkedAnswer
+        bool[questionCount] = 0;
+    }
+    else{
+        for(let i=0 ;i< labels.length;i++)
+        {
+            if(labels[i].htmlFor === checkedAnswer)
+            {
+                userAns[questionCount] = labels[i].innerHTML
+               
+            }
+        }
+        
+    }
+    console.log(userAns[questionCount]);
+
+    
+    // console.log(checkedAnswer);
 
     if(checkedAnswer === quizDb[questionCount].ans){
         score++;
+        bool[questionCount] = 1;
+    }
+    else{
+        bool[questionCount] = 0;
     }
 
+    
     questionCount++;
     deselectAll();
     if(questionCount < quizDb.length){
         loadQuestion();
     }
     else{
+        questionArea.classList.add('questionArea');
+        result.classList.remove('result');
+        displayData();
         showScore.innerHTML = `
             <h3> You Scored ${score}/${quizDb.length}</h3>
             <button class = "btn" onclick = "location.reload()">Play Again</button>
-        `
+        `;    console.log(bool)
+                console.log(userAns)
 
         showScore.classList.remove('scoreArea')
     }
@@ -129,7 +286,7 @@ submit_login_form.addEventListener('click' , ()=>{
     if(uname.value === 'admin' && psw.value === 'admin'){
         rulesArea.classList.remove('rulesArea');
         loginArea.style.display = 'none';
-       // alert('login success');
+    //    alert('login success');
     }
     else{
         alert('login failed');
@@ -137,14 +294,25 @@ submit_login_form.addEventListener('click' , ()=>{
 
 })
 
+agree.addEventListener('click',()=>{
+    if(agree.checked){
+        document.querySelector(".iAgree").style.background = '';
+    }
+    else{
+        document.querySelector(".iAgree").style.background = '#f2dede';
+    }    
+})
+
 startQuiz.addEventListener('click' , ()=> {
     if(agree.checked){
         console.log('checked');
         questionArea.classList.remove('questionArea');
         rulesArea.classList.add('rulesArea');
+        document.querySelector(".iAgree").style.background = '';
 
     }
     else{
+        
         console.log('unchecked');
     }
 })
